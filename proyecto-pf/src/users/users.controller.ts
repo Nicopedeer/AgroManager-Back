1b77b5f525a818c4452d8d6739977e96ed9e539d
JuanGom01 <164223994+JuanGom01@users.noreply.github.com>
2024-07-24 12:45:01 -0300

    user crud documentation

--- proyecto-pf/src/users/users.controller.ts
+++ proyecto-pf/src/users/users.controller.ts
@@ -9,7 +9,11 @@ import { roleGuard } from 'src/auth/guards/roles.guard';
 import { RolesEnum } from './entities/roles.entity';
 import { User } from './entities/user.entity';
 import { ChangePasswordDto } from './dto/change-password.dto';
+import { changePasswordDecorator, deleteUserDecorator, getUserByIdDecoractor, getUserDecorator, updateUserDecorator } from './user.decorators';
+import { ApiTags } from '@nestjs/swagger';
 
+
+@ApiTags("users")
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -22,26 +26,31 @@ export class UsersController {
   }
 
   @Get()
+  @getUserDecorator()
   getUsersPage(@Query("page") page: string = "1", @Query("limit") limit: string = "5") {
     return this.usersService.getUsersPage(Number(page), Number(limit))
   }
 
   @Get(':id')
+  @getUserByIdDecoractor()
   getUserById(@Param('id', ParseUUIDPipe) id: UUID) {
     return this.usersService.getUserById(id);
   }
 
   @Put("password/:id")
+  @changePasswordDecorator()
   changePassword(@Param("id", ParseUUIDPipe) id: UUID, @Body() changePasswordDto: ChangePasswordDto) {
     return this.usersService.changePasword(id, changePasswordDto)
   }
 
   @Put(':id')
+  @updateUserDecorator()
   updateUser(@Param('id', ParseUUIDPipe) id: UUID, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.updateUser(id, updateUserDto);
   }
 
   @Delete(':id')
+  @deleteUserDecorator()
   deleteUser(@Param('id', ParseUUIDPipe) id: UUID) {
     return this.usersService.deleteUser(id);
   }
