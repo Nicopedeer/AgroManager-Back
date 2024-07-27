9054e83d5271d86a71e59e33d52d4db0973d7627
NicolasPando <nicolas.g.pando@gmail.com>
2024-07-27 13:01:18 -0300

    fix

--- proyecto-pf/src/main.ts
+++ proyecto-pf/src/main.ts
@@ -4,6 +4,7 @@ import { loggerMiddleware } from './middlewares/logger.middleware';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
+const PORT = process.env.DB_port
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,6 +24,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig)
   SwaggerModule.setup("api", app, document)
 
-  await app.listen(3000);
+  await app.listen(PORT || 3000);
 }
 bootstrap();
