9290b13c60efa4379763f4340d684ce5b5d6762a
Nicolas Pedernera <nicopedernera@hotmail.com.ar>
2024-08-04 17:58:20 -0300

    ContactMail-Created

--- proyecto-pf/src/email/email.module.ts
+++ proyecto-pf/src/email/email.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { EmailsService } from './email.service';
 import { EmailsController } from './email.controller'; 
+import { UsersRepository } from 'src/users/users.repository';
+import { User } from 'src/users/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Role } from 'src/users/entities/roles.entity';
+
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forFeature([User, Role])],
     controllers: [EmailsController],
-    providers: [EmailsService],
+    providers: [EmailsService, UsersRepository],
 })
 export class EmailsModule {}
